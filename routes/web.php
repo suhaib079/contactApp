903c8f98530a96b0bd1b0eb5b32b136b51193b2d
suhaib079 <suhaibabdennabi33@gmail.com>
2023-03-07 22:28:22 +0300

    adding auth and table

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/contacts', function () {
     return view('contacts.index');
 });
+    
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
