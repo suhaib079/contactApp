4e9d66eafb83da0a62ead3e5cded0b04f0bbc806
suhaib079 <suhaibabdennabi33@gmail.com>
2023-03-07 21:03:38 +0300

    create layout

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.layout')
+
 @section('content')
     
-<h1>welcome</h1>
+<h1>all contact page</h1>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/contacts', function () {
+    return view('contacts.index');
+});
